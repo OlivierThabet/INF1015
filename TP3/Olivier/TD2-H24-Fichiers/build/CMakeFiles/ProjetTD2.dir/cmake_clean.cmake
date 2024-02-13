bf754d16ba592ec5648ce0e16ae22488451ba7d1
OlivierThabet <olivier.thabet@polymtl.ca>
2024-02-13 16:10:10 -0500

    tp3 initial commit

---
+++ TP3/Olivier/TD2-H24-Fichiers/build/CMakeFiles/ProjetTD2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetTD2.dir/bibliotheque_cours/bibliotheque_cours.cpp.obj"
+  "CMakeFiles/ProjetTD2.dir/bibliotheque_cours/bibliotheque_cours.cpp.obj.d"
+  "CMakeFiles/ProjetTD2.dir/bibliotheque_cours/verification_allocation.cpp.obj"
+  "CMakeFiles/ProjetTD2.dir/bibliotheque_cours/verification_allocation.cpp.obj.d"
+  "CMakeFiles/ProjetTD2.dir/td2.cpp.obj"
+  "CMakeFiles/ProjetTD2.dir/td2.cpp.obj.d"
+  "ProjetTD2.exe"
+  "ProjetTD2.exe.manifest"
+  "ProjetTD2.pdb"
+  "libProjetTD2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetTD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
